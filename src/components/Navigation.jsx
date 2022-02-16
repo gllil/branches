1c588925b570286740733d4794f479e37c6b28d1
Gary Llil <garyjllil@gmail.com>
2022-02-16 02:00:42 -0500

    changed button color

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,7 +6,7 @@ const Navigation = () => {
         <h3 class="text-3xl font-bold font-arch ml-2">Branches</h3>
       </div>
       <div class="flex justify-end items-center">
-        <button type="button" class="text-center text-white bg-purple-900 py-2 px-2 font-conf rounded-xl mt-2 mr-2">
+        <button type="button" class="text-center text-white bg-indigo-900 py-2 px-2 font-conf rounded-xl mt-2 mr-2">
           Login
         </button>
       </div>
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -8,7 +8,7 @@ const Landing = () => {
         <div class="ml-2">
           <h3 class="text-4xl font-bold font-conf">Connect and Grow your Branches</h3>
           <h3 class="text-3xl font-arch mt-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
-          <button class="text-center text-white font-bold bg-purple-900 py-2 px-2 font-conf rounded-xl mt-5">Create a free account</button>
+          <button class="text-center text-white bg-indigo-900 py-2 px-2 font-conf rounded-xl mt-5">Create a free account</button>
         </div>
         <div class="flex-wrap justify-items-center"><img class="opacity-90 max-w-lg mx-auto my-auto" src={leaves}/></div>
         
